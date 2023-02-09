3d1b107f3ec6e3f0e6ccf4816fc59502958d92d6
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-02-09 17:50:26 -0300

    test mantine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,15 @@
 import React, { useState, useEffect } from "react";
-
 import axios from "axios";
+
+import { SideBar } from "./components/SideBar";
 
 
 function App() {
-
-  const [links, setlinks] = useState([]);
-
-
-  //fetch the links from the api
-  const getLinks = () => {
-    axios.get("http://localhost:3000/links")
-      .then((res) => {
-        console.log("data fetched")
-        setlinks(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  //useEffect
-  useEffect(() => {
-    getLinks();
-    console.log(links);
-  }, []);
-
-
   return (
     
-    <div className="links">
-      {links.map((todo) => (
-
-        <p>{todo.text}</p>
-
-      ))}
-    </div>
+    <section id="app">
+      <SideBar/>
+    </section>
 
   );
 }
