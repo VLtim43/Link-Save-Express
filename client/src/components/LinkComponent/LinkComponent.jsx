f462c814e417b0c431af6ae3293f65c97145b3e8
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-02-11 20:08:52 -0300

    link truncate

--- client/src/components/LinkComponent/LinkComponent.jsx
+++ client/src/components/LinkComponent/LinkComponent.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 export const LinkComponent = ({text, label}) => {
 
-  function TruncateLink(link) {
-    const maxLength = 115
-    const truncatedLink = link.length > maxLength
-      ? link.substr(0, maxLength) + '...'
-      : link;
-  
-    return <a href={link} target="_blank">{truncatedLink}</a>;
-  }
+  function TruncateLink(link ) {
+  const [truncatedLink, setTruncatedLink] = useState(link);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const maxLength = window.innerWidth < 1800 ? 110 : 155;
+      setTruncatedLink(link.length > maxLength
+        ? link.substr(0, maxLength) + '...'
+        : link
+      );
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [link]);
+
+  return <a href={link} target="_blank">{truncatedLink}</a>;
+}
   
 
   return (
